76821e66af88b6c2381e4f6bd2eea3052c85c4d7
nichnikov <a.nichnikov@gmail.com>
2023-12-12 11:50:29 +0300

    scripts ref

--- pipline.py
+++ pipline.py
@@ -8,19 +8,17 @@ from src.config import logger
 from src.storage import ElasticClient
 
 
-es = ElasticClient()
 
-queries_index = "dataset_queries"
-answers_index = "dataset_answers"
-
-for index in [queries_index, answers_index]:
-    
+def es_update(index: str, data: [{}]):
+    es = ElasticClient()
+       
     loop = asyncio.get_event_loop()
     
     try:
         loop.run_until_complete(es.delete_index(index))
     except:
-        logger.exception("There no index {}".format(index))
+        pass
+        # logger.exception("There no index {}".format(index))
 
     try:
         loop.run_until_complete(es.create_index(index))
@@ -28,24 +26,37 @@ for index in [queries_index, answers_index]:
         pass
         # logger.exception("There no index {}".format(index))
 
-loop.close()
+    try:
+        loop.run_until_complete(es.add_docs(index, data))
+    except:
+        logger.info("There is problem with sending queries to elastic for Sys {}".format(str(sys_id)))
 
-sys_ids = [1, 14, 15, 2, 10, 8, 3]
+    logger.info("Queries for sys ID {} send to Elastic".format(str(sys_id)))
+    
+    loop.close()
+    
 
 
-''
+queries_index = "dataset_queries"
+answers_index = "dataset_answers"
+
+
 try:
-    answers_file_path = os.path.join(os.getcwd(), "data", "dataset_answers.csv")
+    answers_file_path = os.path.join(os.getcwd(), "data", "231207", "dataset_answers.csv")
     os.remove(answers_file_path)
 except:
     print("There not file dataset_answers.csv")
 
 try:
-    queries_file_path = os.path.join(os.getcwd(), "data", "dataset_queries.csv")
+    queries_file_path = os.path.join(os.getcwd(), "data", "231207", "dataset_queries.csv")
     os.remove(queries_file_path)
 except:
     print("There not file dataset_queries.csv")
 
+
+
+
+sys_ids = [1, 14, 15, 2, 10, 8, 3]
 for sys_id in sys_ids:
     data_upload.sys_data_upload(sys_id)
     logger.info("data uploading for sys ID {}".format(str(sys_id)))
@@ -55,7 +66,7 @@ for sys_id in sys_ids:
     logger.info("answers lematized for sys ID {}".format(str(sys_id)))
 
     """добавим лемматизированные ответы и запишем в файл:"""
-    answers_dicts = [{**d, **{"LemAnswer": la}} for la, d in zip(lem_answers, answers_dicts)]
+    answers_dicts = [{**d, **{"LemAnswer": la}, **{"sys_id": sys_id}} for la, d in zip(lem_answers, answers_dicts)]
     answers_df = pd.DataFrame(answers_dicts)
     
     print("answers до удаления пустых ячеек:", answers_df.shape)
@@ -64,16 +75,6 @@ for sys_id in sys_ids:
     
     answers_df.to_csv(os.path.join(os.getcwd(), "data", "231207", "dataset_answers.csv"), sep="\t", index_label=False, index=False, mode="a")
     logger.info("Dataframe with answers for sys ID {} saved".format(str(sys_id)))
-    
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(es.add_docs(answers_index, answers_dicts))
-        loop.close()
-    except:
-        loop.close()
-        logger.info("There is problem with sending answers to elastic for Sys {}".format(str(sys_id)))
-    
-    logger.info("Answers for sys ID {} send to Elastic".format(str(sys_id)))
     
     """
     Оставим только непохожие вопросы, в количестве
@@ -94,15 +95,6 @@ for sys_id in sys_ids:
         if len(quries) <= 300:
             queries_dicts += [{"sys": sys_id, "id": fa_id, "query": q.query, "lem_query": q.lem_query, } for q in queries_analysis(10, 0.9, quries)]
     
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(es.add_docs(queries_index, queries_dicts))
-        loop.close()
-    except:
-        loop.close()
-        logger.info("There is problem with sending queries to elastic for Sys {}".format(str(sys_id)))
-    
-    logger.info("Queries for sys ID {} send to Elastic".format(str(sys_id)))
     
     queries_for_train_df = pd.DataFrame(queries_dicts)
     print("clusters до удаления пустых ячеек:", queries_for_train_df.shape)
