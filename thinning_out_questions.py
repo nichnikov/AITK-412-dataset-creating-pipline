996b08fba3eb24c41d33041027cfea39c1c31865
nichnikov <a.nichnikov@gmail.com>
2024-02-12 20:14:24 +0300

    ref

--- thinning_out_questions.py
+++ thinning_out_questions.py
@@ -10,7 +10,7 @@ from src.start import queries_analysis
 
 clusters_df = pd.read_feather(os.path.join("data", "240212", "all_clusters.feather"))
 print(clusters_df)
-clusters_df = clusters_df[:10000]
+# clusters_df = clusters_df[:10000]
 sys_ids = clusters_df["SysID"].unique()
 
 queries_dicts = []
@@ -23,8 +23,7 @@ for sys_id in sys_ids:
         if len(quries) <= 300:
             queries_dicts += [{"sys": sys_id, "id": fa_id, "query": q.Cluster, "lem_query": q.LemCluster, } for q in queries_analysis(30, 0.9, quries)]
 
-print(queries_dicts[:10])
-print(len(queries_dicts))
+
 queries_df = pd.DataFrame(queries_dicts)
 print(queries_df)
 queries_df.to_feather(os.path.join("data", "240212", "thinning_clusters.feather"))
