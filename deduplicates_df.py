5c506f300b1c922dafbad08912b355d4715e6013
nichnikov <a.nichnikov@gmail.com>
2024-02-14 00:13:51 +0300

    ref

--- deduplicates_df.py
+++ deduplicates_df.py
@@ -1,10 +1,10 @@
 import os
 import pandas as pd
 
-df = pd.read_feather(os.path.join("data", "240212", "thinning_clusters.feather"))
+df = pd.read_feather(os.path.join("data", "240212", "thinning_clusters_2.feather"))
 print(df)
 
 df_unique = df[["sys", "id", "query", "lem_query"]].drop_duplicates()
 print(df_unique)
-df_unique.to_feather(os.path.join("data", "240212", "thinning_clusters_unique.feather"))
-df_unique.to_csv(os.path.join("data", "240212", "thinning_clusters_unique.csv"), sep="\t", index=False)
+df_unique.to_feather(os.path.join("data", "240212", "thinning_clusters_unique_2.feather"))
+df_unique.to_csv(os.path.join("data", "240212", "thinning_clusters_unique_2.csv"), sep="\t", index=False)
